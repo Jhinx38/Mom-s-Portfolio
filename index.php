acf0ee794e35d81b47e623d9ff6c27e83b2b40cd
hakim38 <hakimzack42@gmail.com>
2024-02-17 15:49:13 +0800

    okeii

--- index.php
+++ index.php
@@ -10,12 +10,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script></script>
 
-    <?php include 'sidebar.php'; ?>
 </head>
 
 <body>
     <!-- This is the imprted Boostrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+    <nav class="navbar sticky-top bg-body-tertiary">
+        <div class="container-left">
+            <button><a class="navbar-brand" href="#">EZ Empire</a></button>
+            <button><a class="navbar-brand" href="#">Fixed top</a></button>
+            <button><a class="navbar-brand" href="#">Profile</a></button>
+            <button> <a class="navbar-brand" href="#">Projects</a></button>
+            <button><a class="navbar-brand" href="#">Contact Me</a></button>
+        </div>
+    </nav>
 
 </body>
 
