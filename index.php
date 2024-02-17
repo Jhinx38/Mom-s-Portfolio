e30305504eae35dd0299aacf4a150645cf8059d4
hakim38 <hakimzack42@gmail.com>
2024-02-17 15:23:50 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+
+    <!-- This is the imprted Boostrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script></script>
+
+    <?php include 'sidebar.php'; ?>
+</head>
+
+<body>
+    <!-- This is the imprted Boostrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,59 @@
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+
+        <ul class="sidebar-nav" id="sidebar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="index-admin.php">
+                    <i class="bi bi-grid-fill"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li><!-- End Dashboard Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-file-earmark-text-fill"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="admin-add-itcomm.php">
+                            <i class="bi bi-circle"></i><span>Add IT Committee</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="admin-add-mosque.php">
+                            <i class="bi bi-circle"></i><span>Add Mosque</span>
+                        </a>
+                    </li>
+                </ul>
+            </li><!-- End Form Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="admin-booking-list.php">
+                    <i class="bi bi-collection-fill"></i>
+                    <span>Booking List</span>
+                </a>
+            </li><!-- End Booking List Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="admin-complaint-list.php">
+                    <i class="bi bi-envelope-fill"></i>
+                    <span>Complaint List</span>
+                </a>
+            </li><!-- End Complaint List Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="admin-mosque-list.php">
+                    <i class="bi bi-triangle-fill"></i>
+                    <span>Mosque List</span>
+                </a>
+            </li><!-- End Mosque List Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="admin-itcomm-list.php">
+                    <i class="bi bi-person-fill"></i>
+                    <span>IT Committee List</span>
+                </a>
+            </li><!-- End ITComm List Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="admin-user-list.php">
+                    <i class="bi bi-people-fill"></i>
+                    <span>User List</span>
+                </a>
+            </li><!-- End User List Nav -->
+    </aside><!-- End Sidebar-->
